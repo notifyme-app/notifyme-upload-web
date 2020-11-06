b6f1ca9698a0155d83200223c5f25123b1fd1c40
Sven Kellenberger <kellenberger@ubique.ch>
2020-11-06 16:37:11 +0100

    add success and error handling

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import './styles/main.scss'
 import mdtoast from '@dmuy/toast'
 
 let upload = async () => {
+    document.getElementById('upload-btn').disabled = true;
+
     const tracePayload = window.location.hash.slice(1);
     console.log("Trace Payload: " + tracePayload);
     const startTime = document.getElementById('start-time').value;
     const endTime = document.getElementById('end-time').value;
-    console.log("Start time: " + startTime  + " End time: " + endTime);
-    
-    if(!startTime || !endTime) {
-        mdtoast("Please fill out both fields", {type: "WARNING"});
+    console.log("Start time: " + startTime + " End time: " + endTime);
+
+    if (!startTime || !endTime) {
+        mdtoast("Please fill out both fields", { type: "WARNING" });
         return;
     }
 
@@ -23,9 +25,16 @@ let upload = async () => {
         method: 'POST',
         mode: 'no-cors',
         body: formData
-      }).then(function(response) {
-        console.log(response.ok);
-      });
+    }).then(function (response) {
+        if(response.ok) {
+            document.getElementById('form-wrapper').style.display = "none";
+            document.getElementById('success-message').style.display = "block";
+        } else {
+            mdtoast("Upload failed", { type: "WARNING" });
+        }
+
+        document.getElementById('upload-btn').disabled = false;
+    });
 };
 
 let ready = (fn) => {
